0f830b454b3343c12be95c264c627ae83e395611
VanekAndreev2005 <118421152+VanekAndreev2005@users.noreply.github.com>
2022-11-24 15:44:49 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User()
+            {
+                FirstName = "Андреев",
+                Name = "Иван",
+                Login = "Admin",
+                Password = "admin",
+            };
+            /*Console.WriteLine("Введите логин");
+            string login = Console.ReadLine();
+            user.AuthorizeUser(login, "");*/
+            Console.WriteLine(user.Name);
+            Console.WriteLine("Введите имя");
+            user.Name = Console.ReadLine();
+            Console.WriteLine($"Ваше новое имя - {user.Name}");
+            Console.ReadLine();
+        }
+    }
+
+    class User
+    {
+        public string FirstName;
+        public string Name;
+
+        public string Login;
+        public string Password;
+
+        public void AuthorizeUser(string login, string password)
+        {
+            if(login == Login)
+            {
+                Console.WriteLine("Вы успешно авторизовались");
+            }
+            else
+            {
+                Console.WriteLine("Авторизация не удалась");
+            }
+        }
+    }
+}
